084e5b8451620dd9b9852808628037c1ef6685fe
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-08-07 09:45:20 +0500

    bajarildi

--- file03.py
+++ file03.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    l=[]
+    i=0
+    while i<len(data):
+        if data[i].isdigit():
+            l.append(data[i])
+        i+=1
+    return l
 # Read data from file
+f=open("data/data03.txt").read()
+print(main(f))
+
